fa9f3e1ffc49315ff9ee9a9617608352d441e6d1
Eladio Castañeda <eladio.dev@gmail.com>
2024-11-03 06:03:28 -0400

    hello word en pagina inicio se uso copilot para solucionar que no arrancaba

---
+++ CURSO REACT UDEMY Pre-WEB/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App.jsx'; // Asegúrate de que esta línea apunte al archivo correcto
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
